a812344fb22c7559165a0afc7a1c302dd36ef75c
educationeating <79743110+educationeating@users.noreply.github.com>
2021-06-01 20:27:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+$(window).on('load',function(){
+
+            $(".loader").fadeOut(1000);
+
+            $(".content").fadeIn(1000);
+        });
+        var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "60%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
